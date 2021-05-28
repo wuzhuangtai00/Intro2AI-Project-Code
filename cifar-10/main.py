74864e03421ecc13f365b52ce281fc6ce7e86937
wuzhuangtai00 <wuzhuangtai00@foxmail.com>
2021-05-29 01:02:53 +0800

    Debug WideResNet

--- cifar-10/main.py
+++ cifar-10/main.py
@@ -61,7 +61,7 @@ class Trainer():
                 # print(label)
                 # predict_label = output[label]
                 # print(predict_label)
-                print(label.size())
+                print(label.size()[0])
                 print(output.size())
                 # for i in range(label.size()):
                     # print(i)
