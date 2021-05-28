8c5a31fe7379c73a19690efcfeb6c906b3886559
wuzhuangtai00 <wuzhuangtai00@foxmail.com>
2021-05-29 02:15:13 +0800

    Debug WideResNet

--- cifar-10/main.py
+++ cifar-10/main.py
@@ -76,6 +76,7 @@ class Trainer():
                     print(val_other, val_l)
                     dataset_size += 1
                     output_margin_test += max(0, val_l - val_other)
+                    print(output_margin_test)
 
 
                 # print(output)
