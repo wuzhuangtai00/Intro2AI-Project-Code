79354043597f5c4ceba6d3b89c0d122b91fb9374
wuzhuangtai00 <wuzhuangtai00@foxmail.com>
2021-05-30 00:20:48 +0800

    debug MNIST

--- mnist/main.py
+++ mnist/main.py
@@ -8,8 +8,8 @@ import torchvision as tv
 
 from time import time
 from src.model import Model
-from attack import FastGradientSignUntargeted
-from utils import makedirs, create_logger, tensor2cuda, numpy2cuda, evaluate, save_model
+from src.attack import FastGradientSignUntargeted
+from src.utils import makedirs, create_logger, tensor2cuda, numpy2cuda, evaluate, save_model
 
 from argument import parser, print_args
 
