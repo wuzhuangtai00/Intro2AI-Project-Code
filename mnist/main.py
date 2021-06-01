f45c6b63d2e9a1b2bed4113da299bdc37682387f
吴瑾昭 <scape@wujinzhaos-MacBook-Air.local>
2021-06-01 21:12:00 +0800

    debugg computing margin

--- mnist/main.py
+++ mnist/main.py
@@ -87,7 +87,7 @@ class Trainer():
                     cx = data[i].clone()
                     cy = label[i].item()
                     cx = cx.unsqueeze(dim = 0)
-                    cy = cy.unsqueeze(dim = 0)
+                    # cy = cy.unsqueeze(dim = 0)
                     # print(data.size())
                     # print(cx.size())
                     all_layer_margin_test += compute_all_layer_margin(self, model, cx, cy)
@@ -187,7 +187,7 @@ class Trainer():
                     cx = data[i].clone()
                     cy = label[i]
                     cx = cx.unsqueeze(dim = 0)
-                    cy = cy.unsqueeze(dim = 0)
+                    # cy = cy.unsqueeze(dim = 0)
                     total_all_layer += compute_all_layer_margin(self, model, cx, cy)
 
                     x = output[i].clone()
