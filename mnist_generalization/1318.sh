2be1c50bd8c99a7a5bc85a9c851308c4aff23599
Scape <wuzhuangtai00@foxmail.com>
2021-06-08 02:10:46 +0800

    debug

--- mnist_generalization/1318.sh
+++ mnist_generalization/1318.sh
@@ -1,1 +1,1 @@
-python  main.py --data_root ~/datasets -e 0.314 -p 'linf' --adv_train --affix 'linf'  --batch_size 1 --epoch 400 --model_root ~/checkpoint
+python  main.py --data_root ~/datasets -e 0.314 -p 'linf' --adv_train --affix 'linf'  --batch_size 1 --max_epoch 400 --model_root ~/checkpoint
