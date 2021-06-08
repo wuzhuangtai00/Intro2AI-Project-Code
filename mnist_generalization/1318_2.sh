1b42b0d52a50f8c4d598ea91754fec647cee24d6
Scape <wuzhuangtai00@foxmail.com>
2021-06-08 10:18:26 +0800

    training

--- mnist_generalization/1318_2.sh
+++ mnist_generalization/1318_2.sh
@@ -1,1 +1,1 @@
-python  main.py --data_root ~/datasets -e 0.314 -p 'l2' --adv_train --affix 'l2' --batch_size 1 --model_root ~/checkpoint --max_eopch 400
+python  main.py --data_root ~/datasets -e 0.314 -p 'l2' --adv_train --affix 'l2' --batch_size 1 --model_root ~/checkpoint --max_epoch 400
