4e2a0b1a099748844f8915781bf780560de152d4
Scape <wuzhuangtai00@foxmail.com>
2021-06-08 10:16:19 +0800

    training

--- mnist_generalization/matpool.sh
+++ mnist_generalization/matpool.sh
@@ -1,3 +1,2 @@
 python  main.py --data_root ~/datasets --affix std --batch_size 1
-python  main.py --data_root ~/datasets -e 0.157 -p 'linf' --adv_train --affix 'linf'  --batch_size 1
-python  main.py --data_root ~/datasets -e 0.314 -p 'l2' --adv_train --affix 'l2' --batch_size 1
+python  main.py --data_root ~/datasets -e 0.157 -p 'linf' --adv_train --affix 'linf'  --batch_size 400
