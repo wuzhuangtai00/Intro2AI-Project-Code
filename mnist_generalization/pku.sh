fd35424f09d1cf009d20a78071b1148c8b3b2e8f
Scape <wuzhuangtai00@foxmail.com>
2021-06-08 22:41:32 +0800

    modify

--- mnist_generalization/pku.sh
+++ mnist_generalization/pku.sh
@@ -1,1 +1,1 @@
-python  main.py --data_root /datasets_general -e 0.157 -p 'linf' --adv_train --affix 'linf'  --batch_size 1
+python  main.py --data_root /datasets_general -e 0.314 -p 'l2' --adv_train --affix 'linf'  --batch_size 1
