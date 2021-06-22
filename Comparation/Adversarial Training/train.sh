91569bc9b4891ad286fcf4fdb561c4f78ee1d514
Scape <wuzhuangtai00@foxmail.com>
2021-06-23 01:10:04 +0800

    cifar10

--- Comparation/Adversarial Training/train.sh
+++ Comparation/Adversarial Training/train.sh
@@ -1,2 +1,2 @@
-python main.py --data_root '~/datasets' -e 0.0157 -p 'linf' --adv_train --affix 'linf' --batch_size 1024
+python main.py --data_root '~/datasets' -e 0.0157 -p 'linf' --adv_train --affix 'linf' --batch_size 512
 
